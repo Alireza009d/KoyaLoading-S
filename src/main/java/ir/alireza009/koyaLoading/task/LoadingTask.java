7d6b7830b6140f650866fec739a8729ae6252545
Alireza009d <deleted008user@gmail.com>
2025-02-27 23:50:57 +0330

    v1.0.3

--- src/main/java/ir/alireza009/koyaLoading/task/LoadingTask.java
+++ src/main/java/ir/alireza009/koyaLoading/task/LoadingTask.java
@@ -1,13 +1,13 @@
 package ir.alireza009.koyaLoading.task;
 
 import ir.alireza009.koyaLoading.storage.Storage;
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import ir.alireza009.koyaLoading.KoyaLoading;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class LoadingTask {
@@ -28,14 +28,13 @@ public class LoadingTask {
 
     public static void loadingTask(Player player, Location location, int delay) {
         player.setGameMode(GameMode.SPECTATOR);
-        Location loadingLocation = new Location(location.getWorld(), location.getX(), location.getY()+45, location.getZ(), 0.0F, 90.0F);
-        player.teleport(loadingLocation);
-
-        int[] teleportTimes = {40, 70, 100, 130, 160, 190, 220, 250, 280};
-
+        player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 999, 1, false, false));
+        final Location[] loadingLocation = {new Location(location.getWorld(), location.getX(), location.getY() + 45, location.getZ(), 0.0F, 90.0F)};
+        int y = countSolidBlocks(location, loadingLocation[0]);
+        loadingLocation[0].add(0, y, 0);
+        player.teleport(loadingLocation[0]);
         new BukkitRunnable() {
             int time = 0;
-            int index = 0;
 
             @Override
             public void run() {
@@ -43,31 +42,75 @@ public class LoadingTask {
                     cancel();
                     return;
                 }
-
+                if (time == 1) player.setGameMode(GameMode.SPECTATOR);
                 time++;
 
-                if (index < teleportTimes.length && time == teleportTimes[index]) {
-                    if (time == 280) {
-                        player.setGameMode(GameMode.SURVIVAL);
-                        Storage.getLocation().remove(player.getUniqueId());
-                        config.set("Locations." + player.getName(), null);
-                        KoyaLoading.getInstance().saveConfig();
-                        loadingLocation.add(0, -4.5, 0);
-                        loadingLocation.setPitch(0.0F);
-                        loadingLocation.setYaw(90.0F);
-                    } else {
-                        loadingLocation.add(0, -5, 0);
-                    }
-
-                    player.teleport(loadingLocation);
-                    playTeleportSound(player);
-                    index++;
+                if ((loadingLocation[0].getBlockY() - location.getBlockY()) < 10) {
+                    player.teleport(location);
+                    player.setGameMode(GameMode.SURVIVAL);
+                    player.removePotionEffect(PotionEffectType.INVISIBILITY);
+                    Storage.getLocation().remove(player.getUniqueId());
+                    config.set("Locations." + player.getName(), null);
+                    KoyaLoading.getInstance().saveConfig();
+                    cancel();
+                    return;
                 }
 
-                if (time >= 280) {
-                    cancel();
+                if ((time % 30) == 0) {
+                    loadingLocation[0].add(0, -5, 0);
+                    if (loadingLocation[0].getBlock().getType().isSolid()) loadingLocation[0] = getSafeLocation(loadingLocation[0]);
+                    player.teleport(loadingLocation[0]);
+                    playTeleportSound(player);
+
                 }
             }
         }.runTaskTimer(KoyaLoading.getInstance(), delay, 1);
     }
+
+    private static Location getSafeLocation(Location loc) {
+        while (loc.getBlock().getType().isSolid()) {
+            loc.add(0, -1, 0);
+        }
+        return loc;
+    }
+
+    private static int countSolidBlocks(Location loc1, Location loc2) {
+        World world = loc1.getWorld();
+        int count = 0;
+
+        if (loc1.getBlockX() != loc2.getBlockX()) {
+            int minX = Math.min(loc1.getBlockX(), loc2.getBlockX());
+            int maxX = Math.max(loc1.getBlockX(), loc2.getBlockX());
+
+            for (int x = minX; x <= maxX; x++) {
+                Block block = world.getBlockAt(x, loc1.getBlockY(), loc1.getBlockZ());
+                if (!block.isEmpty()) {
+                    count++;
+                }
+            }
+        } else if (loc1.getBlockY() != loc2.getBlockY()) {
+            int minY = Math.min(loc1.getBlockY(), loc2.getBlockY());
+            int maxY = Math.max(loc1.getBlockY(), loc2.getBlockY());
+
+            for (int y = minY; y <= maxY; y++) {
+                Block block = world.getBlockAt(loc1.getBlockX(), y, loc1.getBlockZ());
+                if (!block.isEmpty()) {
+                    count++;
+                }
+            }
+        } else if (loc1.getBlockZ() != loc2.getBlockZ()) {
+            int minZ = Math.min(loc1.getBlockZ(), loc2.getBlockZ());
+            int maxZ = Math.max(loc1.getBlockZ(), loc2.getBlockZ());
+
+            for (int z = minZ; z <= maxZ; z++) {
+                Block block = world.getBlockAt(loc1.getBlockX(), loc1.getBlockY(), z);
+                if (!block.isEmpty()) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
 }
